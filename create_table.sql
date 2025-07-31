f8ffb247b1791149e7fee77c83fff9f25ff6dfb2
Akhilm234 <152249709+Akhilm234@users.noreply.github.com>
2025-07-31 11:43:00 +0530

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,11 @@
+Create Table products(
+    id integer primary key,
+    cost real,
+    category text,
+    name text,
+    brand text,
+    reatail_price real,
+    department text,
+    sku text,
+    distribution_center_id integer
+);
